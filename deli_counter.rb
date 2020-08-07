dd51c809a1044133bb5a8c3dcbe7a35b91040e41
Sara A <calisaraalhindi@gmail.com>
2020-08-07 18:17:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,8 @@ def line(guests)
   current_line = []
   if guests.length == 0
     puts "The line is currently empty."
-    else guests.each_with_index(1) do { |name,index| }
+    else 
+      guests.each_with_index(1) do { |name,index| }
     current_line >> ("#{index}". "#{name}")
   end
   puts the "line is currently #{current_line.join" ")}"
