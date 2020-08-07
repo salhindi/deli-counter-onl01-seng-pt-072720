c03a2ea25f42f661a9921e6c5f0cbe2f98a8464c
Sara A <calisaraalhindi@gmail.com>
2020-08-07 18:33:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,8 @@ def line(a)
   line_arr = []
   if a.length == 0
     puts "The line is currently empty."
-  else a.each.with.index(1) do |name, index|
+  else 
+    a.each.with.index(1) do |name, index|
     line_arr.push("#{index}. #{name}")
   end
   puts "The line is currently: #{line_arr.join(" ")}."
