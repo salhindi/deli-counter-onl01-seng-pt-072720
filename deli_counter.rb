796a767662c6c1d3418469b8c14757a42edec6e9
Sara A <calisaraalhindi@gmail.com>
2020-08-07 17:46:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(x)
   katzdeli = []
   if x.length == 0
     puts "The line is currently empty."
-    when line.length == 1
+    when x.length == 1
       puts "there are people in the line"
       end
     end
