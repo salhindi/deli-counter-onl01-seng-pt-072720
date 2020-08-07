4e1cdb9ed410c4926c70a80512215e179d269d6c
Sara A <calisaraalhindi@gmail.com>
2020-08-07 17:48:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,2 @@
-# Write your code here.
-def line(x)
-  katzdeli = []
-  if x.length == 0
-    puts "The line is currently empty."
-    if x.length == 1
-      puts "there are people in the line"
-      end
-    end
 
   
