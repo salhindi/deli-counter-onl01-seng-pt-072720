f70867ba987fed8f06a34c5b1ce72c43d073ebda
Sara A <calisaraalhindi@gmail.com>
2020-08-07 17:42:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line(x)
   when line.length == 0
     puts "The line is currently empty."
-    when line.length == >1
+    when line.length == 1
       puts "there are people in the line"
       end
     end
