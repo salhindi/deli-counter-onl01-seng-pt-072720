4ead488765b056e0253fadc83904540f8cd03aae
Sara A <calisaraalhindi@gmail.com>
2020-08-07 18:57:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,15 @@ katz_deli = []
 
 def line(guests)
   arr = []
+  #create array for the line
   if guests.length == 0
     puts "The line is currently empty."
   else 
     guests.each.with_index(1) do |n, idx|
     arr.push("#{idx}. #{n}")
+    #create an if else statement for two scenarios
+    #if it works with different people and the current line, use interpolation
+    #ppl, line number is index and will begin at 1 (n=name, idx =index)
   end
   puts "The line is currently: #{arr.join(" ")}"
 end
